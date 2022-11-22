f664f322e72fa0e4b18b68abe73e08d09e5d0529
Eric Santos <djericsantos1@gmail.com>
2022-11-22 12:59:16 -0800

    Add files via upload

---
+++ IP_address.ps1
@@ -0,0 +1,9 @@
+﻿
+# Script Name: IP_address.ps1
+# Author: Eric Santos
+# Date of last Revision: 11/22/2022
+# Purpose: Displays IP address
+
+ipconfig /all > network_report.txt
+Select-String network_report.txt -Pattern "\d{1,3}(\.\d{1,3}){3}" -Exclude " IPv4 Address. . . . . . . . . . . : 1"| select-object -first 1 
+del network_report.txt
