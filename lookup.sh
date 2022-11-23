e592e03437cada84f6c75a686811bf20b081fd93
Eric Santos <djericsantos1@gmail.com>
2022-11-23 13:12:10 -0800

    Update lookup.sh

--- lookup.sh
+++ lookup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+
+
+# Script Name: lookup.sh
+# Author: Eric Santos
+# Date of last Revision: 11/23/2022
+# Purpose: Makes file with Domain info
+
+
+#If user doesnt put Domain as argument, it asks to put one
 domain=$1
 domain=${1:?"Domain name missing. Enter Domain"}
 
+#Makes TXT file 
 function make_file {
 touch domain_info.txt
 }
